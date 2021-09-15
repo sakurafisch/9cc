7af887504158cc184a93c4bb77a6a8f82a079538
sakurafisch <sakurafisch@gmail.com>
2021-09-15 18:01:35 +0800

    feat: 编译算术表达式

--- main.c
+++ main.c
@@ -6,10 +6,32 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "引数数量错误\n");
 		return 1;
 	}
+
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n", atoi(argv[1]));
+	printf("  mov rax, %d\n", strtol(p, &p, 10));
+
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("  add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if (*p == '-') {
+			p++;
+			printf("  sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "预料之外的文字: '%c'\n", *p);
+
+		return 1;
+	}
+
 	printf("  ret\n");
 	return 0;
 }
